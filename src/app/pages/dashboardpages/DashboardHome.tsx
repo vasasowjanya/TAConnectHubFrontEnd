fc885c6a31fd0ddbd4474e7e8726abc7fd3a40df
rubayet-islam <season.ss2001@gmail.com>
2024-04-24 13:09:47 +0600

    role wise navbar created

---
+++ src/app/pages/dashboardpages/DashboardHome.tsx
@@ -0,0 +1,11 @@
+const DashboardHome = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold text-center">
+        Welcome to TA Connect Hub Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default DashboardHome;
