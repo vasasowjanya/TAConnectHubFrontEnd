2dc0e0c9de6ed23bf30d3b557c77bea896333647
rubayet-islam <season.ss2001@gmail.com>
2024-04-25 18:59:47 +0600

    ta committee done

--- src/app/components/DashboardNavbar.tsx
+++ src/app/components/DashboardNavbar.tsx
@@ -11,7 +11,7 @@ const DashboardNavbar = () => {
     <div>
       <div className="py-5 bg-gray-950 flex items-center justify-center gap-5">
         <img className="w-14 object-cover" src={logo} alt="logo" />
-        <h1 className="text-white text-2xl font-semibold">TA Connect Hub</h1>
+        <h1 className="text-white text-2xl font-semibold">TAConnectHub</h1>
       </div>
       <div>
         {/* @ts-ignore */}
--- src/app/pages/dashboardpages/TaSelection.tsx
+++ src/app/pages/dashboardpages/TaSelection.tsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axiosInstance from "../../../utils/axiosInstance";
+import catchAsync from "../../../utils/catchAsync";
+import toast from "react-hot-toast";
 
 const TaSelection = () => {
+  const [courseData, setCourseData] = useState([]);
+
+  useEffect(() => {
+    const getCourse = async () => {
+      const res = await axiosInstance.get("/applications?get_all=true");
+      if (res.data.success) {
+        setCourseData(res.data.data.data);
+      }
+    };
+    getCourse();
+  }, []);
+
+  const applications =
+    courseData && courseData.filter((course) => course.accepted == "pending");
+
+  console.log(applications);
+
+  const handleAccept = async (id) => {
+    const submitData = {
+      offered: "accepted",
+    };
+
+    catchAsync(async () => {
+      await axiosInstance.patch("/applications/" + id, submitData);
+      toast.success("Offer accepted successfully");
+    })();
+  };
+  const handleReject = async (id) => {
+    const submitData = {
+      offered: "rejected",
+    };
+
+    catchAsync(async () => {
+      await axiosInstance.patch("/applications/" + id, submitData);
+      toast.success("Offer rejected successfully");
+    })();
+  };
+
   return (
     <div>
       <h1 className="text-lg">TA Selection</h1>
@@ -12,6 +54,7 @@ const TaSelection = () => {
               <tr>
                 <th></th>
                 <th>Subject</th>
+                <th>Username</th>
                 <th>Status</th>
                 <th>Action</th>
                 <th>Operation</th>
@@ -19,23 +62,38 @@ const TaSelection = () => {
             </thead>
             <tbody className="text-lg">
               {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Pending</td>
-                <td>
-                  <Link to="/dashboard/ta-application-review/1">
-                    <button className="btn bg-purple-700 hover:bg-purple-800 text-white">
-                      View Details
-                    </button>
-                  </Link>
-                </td>
-                <td>
-                  <button className="btn bg-purple-700 hover:bg-purple-800 text-white">
-                    Accept
-                  </button>
-                </td>
-              </tr>
+              {applications &&
+                applications.map((application, i) => (
+                  <tr key={i}>
+                    <th>{i + 1}</th>
+                    <td>{application?.course?.title}</td>
+                    <td>{application?.ta_applicant?.name}</td>
+                    <td>{application?.accepted}</td>
+                    <td>
+                      <Link
+                        to={`/dashboard/ta-application-review/${application?.id}`}
+                      >
+                        <button className="btn bg-purple-700 hover:bg-purple-800 text-white">
+                          View Details
+                        </button>
+                      </Link>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => handleAccept(application?.id)}
+                        className="btn bg-purple-700 hover:bg-purple-800 text-white"
+                      >
+                        Accept
+                      </button>
+                      <button
+                        onClick={() => handleReject(application?.id)}
+                        className="btn bg-purple-700 hover:bg-purple-800 text-white"
+                      >
+                        Reject
+                      </button>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
