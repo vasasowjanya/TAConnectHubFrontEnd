82e66a566c4159f3153aa54d4cc226176ec37eba
Labib2003 <labib.salimi30@gmail.com>
2024-04-25 18:35:27 +0600

    ta applicant pages done

--- src/app/pages/dashboardpages/AcceptRejectOffers.tsx
+++ src/app/pages/dashboardpages/AcceptRejectOffers.tsx
@@ -1,7 +1,37 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+import { useEffect, useState } from "react";
+import catchAsync from "../../../utils/catchAsync";
+import axiosInstance from "../../../utils/axiosInstance";
+import toast from "react-hot-toast";
+
 const AcceptRejectOffers = () => {
+  const lsUser = localStorage.getItem("user");
+  let user: any;
+  if (lsUser) user = JSON.parse(lsUser);
+
+  const [applications, setApplications] = useState([]);
+  const [refetchTrigger, setRefetchTrigger] = useState(false);
+
+  useEffect(() => {
+    catchAsync(async () => {
+      const res = await axiosInstance.get(
+        `/applications?get_all=true&ta_applicant_id=${user.id}&offered=accepted`,
+      );
+      setApplications(res.data.data.data);
+    })();
+  }, [refetchTrigger, user.id]);
+
+  const refetch = () => {
+    setRefetchTrigger((ps) => !ps);
+  };
+
   return (
     <div>
-      <h1 className="pb-4 text-xl font-semibold text-center">My Available Offers</h1>
+      <h1 className="pb-4 text-xl font-semibold text-center">
+        My Available Offers
+      </h1>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -15,21 +45,62 @@ const AcceptRejectOffers = () => {
             </tr>
           </thead>
           <tbody className="text-lg">
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-              <td className="flex items-center gap-2">
-                <button className="btn bg-purple-700 hover:bg-purple-800 text-white">
-                  Accept
-                </button>
-                <button className="btn bg-purple-700 hover:bg-purple-800 text-white">
-                  Reject
-                </button>
-              </td>
-            </tr>
+            {applications.map((application, index) => (
+              <tr key={application.id}>
+                <th>{index + 1}</th>
+                <td>{application.course.title}</td>
+                <td>{user.ta_applicant.z_id}</td>
+                <td>
+                  <b>Offered: </b>
+                  {application.offered}
+                  <br />
+                  <b>Accepted: </b>
+                  {application.accepted}
+                </td>
+                <td className="flex items-center gap-2">
+                  {application.accepted === "pending" ? (
+                    <>
+                      <button
+                        className="btn bg-purple-700 hover:bg-purple-800 text-white"
+                        onClick={() => {
+                          catchAsync(async () => {
+                            const res = await axiosInstance.patch(
+                              `/applications/${application.id}`,
+                              {
+                                accepted: "accepted",
+                              },
+                            );
+                            toast.success(res.data.message);
+                            refetch();
+                          })();
+                        }}
+                      >
+                        Accept
+                      </button>
+                      <button
+                        className="btn bg-purple-700 hover:bg-purple-800 text-white"
+                        onClick={() => {
+                          catchAsync(async () => {
+                            const res = await axiosInstance.patch(
+                              `/applications/${application.id}`,
+                              {
+                                accepted: "rejected",
+                              },
+                            );
+                            toast.success(res.data.message);
+                            refetch();
+                          })();
+                        }}
+                      >
+                        Reject
+                      </button>
+                    </>
+                  ) : (
+                    application.accepted
+                  )}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- src/app/pages/dashboardpages/MyTaApplicationStatus.tsx
+++ src/app/pages/dashboardpages/MyTaApplicationStatus.tsx
@@ -1,9 +1,35 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+import { useEffect, useState } from "react";
+import catchAsync from "../../../utils/catchAsync";
+import axiosInstance from "../../../utils/axiosInstance";
+
 const MyTaApplicationStatus = () => {
+  const lsUser = localStorage.getItem("user");
+  let user: any;
+  if (lsUser) user = JSON.parse(lsUser);
+
+  const [applications, setApplications] = useState([]);
+  const [refetchTrigger, setRefetchTrigger] = useState(false);
+
+  useEffect(() => {
+    catchAsync(async () => {
+      const res = await axiosInstance.get(
+        `/applications?get_all=true&ta_applicant_id=${user.id}`,
+      );
+      setApplications(res.data.data.data);
+    })();
+  }, [refetchTrigger, user.id]);
+
+  const refetch = () => {
+    setRefetchTrigger((ps) => !ps);
+  };
+
   return (
     <div>
       <div className="overflow-x-auto">
         <table className="table">
-          {/* head */}
           <thead className="bg-green-500 text-white text-lg">
             <tr>
               <th></th>
@@ -12,12 +38,19 @@ const MyTaApplicationStatus = () => {
             </tr>
           </thead>
           <tbody className="text-lg">
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Blue</td>
-            </tr>
+            {applications.map((application, index) => (
+              <tr key={application.id}>
+                <th>{index + 1}</th>
+                <td>{application.course.title}</td>
+                <td>
+                  <b>Offered: </b>
+                  {application.offered}
+                  <br />
+                  <b>Accepted: </b>
+                  {application.accepted}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
